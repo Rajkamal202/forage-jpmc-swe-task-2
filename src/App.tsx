56dcdb167be364d98ae86dc75cdf8614cc7876b0
Rajkamal202 <raop4903@gmail,com>
2024-04-28 16:27:20 +0530

    Your commit message is here

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import './App.css';
  */
 interface IState {
   data: ServerRespond[],
+  showGraph: boolean,
 }
 
 /**
@@ -22,6 +23,7 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      showGraph: false,
     };
   }
 
@@ -29,19 +31,31 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
+    if (this.state.showGraph){
     return (<Graph data={this.state.data}/>)
+    }
   }
 
-  /**
-   * Get new data from server and update the state with the new data
-   */
-  getDataFromServer() {
+  
+ /**
+ * Get new data from server continuously and update the state with the new data
+ */
+getDataFromServer() {
+  // Define an interval to fetch data every 100ms
+  const intervalID = setInterval(() => {
     DataStreamer.getData((serverResponds: ServerRespond[]) => {
       // Update the state by creating a new array of data that consists of
       // Previous data in the state and the new data from server
       this.setState({ data: [...this.state.data, ...serverResponds] });
     });
-  }
+  }, 100);
+
+  // Set a guard value to stop the interval process if needed
+  setTimeout(() => {
+    clearInterval(intervalID);
+  }, 10000); // Stop fetching data after 10 seconds (adjust as needed)
+}
+
 
   /**
    * Render the App react component
